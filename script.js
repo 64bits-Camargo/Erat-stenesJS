970992e1afde1f0a058f9c8a2bbe626d2bf15533
64bits-Camargo <mateuscamargo0@live.com>
2019-03-06 04:04:52 -0300

    function - identify multiple

--- script.js
+++ script.js
@@ -20,11 +20,35 @@ function createArray(){
     }
 }
 
-// Identify multiple of two
+// Identify multiple 2,3,5,7
 function mult2(){
     for (i = 3; i <= numberTable; i++){
         if (((crivoArray[i] = i) % 2) == 0){
             crivoArray[i] = i + " é múltiplo de 2"
+        }
+    }
+}
+
+function mult3(){
+    for (i = 4; i <= numberTable; i++){
+        if (((crivoArray[i] = i) % 3) == 0){
+            crivoArray[i] = i + " é múltiplo de 3"
+        }
+    }
+}
+
+function mult5(){
+    for (i = 6; i <= numberTable; i++){
+        if (((crivoArray[i] = i) % 5) == 0){
+            crivoArray[i] = i + " é múltiplo de 5"
+        }
+    }
+}
+
+function mult7(){
+    for (i = 8; i <= numberTable; i++){
+        if (((crivoArray[i] = i) % 7) == 0){
+            crivoArray[i] = i + " é múltiplo de 7"
         }
     }
 }
