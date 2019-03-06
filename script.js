975e3cee97d1470f5f450dd09d69a05d99c52a92
64bits-Camargo <mateuscamargo0@live.com>
2019-03-06 05:55:53 -0300

    other function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var rangeNumber = 17
+var rangeNumber = 100
 var crivoArray = []
 const crivoWidth = 10
 const crivoHeight = rangeNumber / crivoWidth
@@ -6,13 +6,31 @@ const numberTable = (crivoWidth * crivoHeight)
 
 
 function start(){
-    createArray()
-    mult2()
-
-    console.table(crivoArray)
+    crivoEros()
 }
 
-
+// write numbers primos
+function crivoEros(){
+    for (i = 0; i <= numberTable; i++){
+        if (((crivoArray[i] = i) % 2) == 0){
+            crivoArray[i] = i
+            console.log(i + " divide por 2")
+        } else if (((crivoArray[i] = i) % 3) == 0){
+            crivoArray[i] = i
+            console.log(i + " divide por 3")
+        } else if (((crivoArray[i] = i) % 5) == 0){
+            crivoArray[i] = i
+            console.log(i + " divide por 5")
+        } else if (((crivoArray[i] = i) % 7) == 0){
+            crivoArray[i] = i
+            console.log(i + " divide por 7")
+        } else {
+            crivoArray[i] = i
+            tablePrimos=(crivoArray[i] = i)
+            console.log(i + " PRIMO")
+        }
+    }
+}
 // Create array with variable rangeNumber
 function createArray(){
     for (i = 1; i <= numberTable; i++){
@@ -20,11 +38,35 @@ function createArray(){
     }
 }
 
-// Identify multiple of two
+// Identify multiple 2,3,5,7
 function mult2(){
-    for (i = 1; i <= numberTable; i++){
+    for (i = 3; i <= numberTable; i++){
         if (((crivoArray[i] = i) % 2) == 0){
             crivoArray[i] = i + " é múltiplo de 2"
+        }
+    }
+}
+
+function mult3(){
+    for (i = 4; i <= numberTable; i++){
+        if (((crivoArray[i] = i) % 3) == 0){
+           crivoArray[i] = i + " é múltiplo de 3"
+        }
+    }
+}
+
+function mult5(){
+    for (i = 6; i <= numberTable; i++){
+        if (((crivoArray[i] = i) % 5) == 0){
+            crivoArray[i] = i + " é múltiplo de 5"
+        }
+    }
+}
+
+function mult7(){
+    for (i = 8; i <= numberTable; i++){
+        if (((crivoArray[i] = i) % 7) == 0){
+           crivoArray[i] = i + " é múltiplo de 7"
         }
     }
 }
